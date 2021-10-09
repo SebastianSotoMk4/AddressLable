17eb12e63843895e8cf8f82953607764f0a2c107
Sebastian Soto <thefirstcoolone@gmail.com>
2021-10-08 18:22:18 -0600

    save

--- AddressLable/AddressLable.Designer.vb
+++ AddressLable/AddressLable.Designer.vb
@@ -22,29 +22,154 @@ Partial Class AddressLable
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.UserInputBox = New System.Windows.Forms.GroupBox()
+        Me.DisplayBox = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.UserInputBox.SuspendLayout()
         Me.SuspendLayout()
         '
-        'GroupBox1
+        'UserInputBox
         '
-        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(235, 438)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.UserInputBox.Controls.Add(Me.Label5)
+        Me.UserInputBox.Controls.Add(Me.TextBox5)
+        Me.UserInputBox.Controls.Add(Me.Label4)
+        Me.UserInputBox.Controls.Add(Me.Label3)
+        Me.UserInputBox.Controls.Add(Me.Label2)
+        Me.UserInputBox.Controls.Add(Me.Label1)
+        Me.UserInputBox.Controls.Add(Me.TextBox4)
+        Me.UserInputBox.Controls.Add(Me.TextBox3)
+        Me.UserInputBox.Controls.Add(Me.TextBox2)
+        Me.UserInputBox.Controls.Add(Me.TextBox1)
+        Me.UserInputBox.Location = New System.Drawing.Point(12, 12)
+        Me.UserInputBox.Name = "UserInputBox"
+        Me.UserInputBox.Size = New System.Drawing.Size(315, 426)
+        Me.UserInputBox.TabIndex = 0
+        Me.UserInputBox.TabStop = False
+        Me.UserInputBox.Text = "User Input"
+        '
+        'DisplayBox
+        '
+        Me.DisplayBox.Location = New System.Drawing.Point(427, 12)
+        Me.DisplayBox.Name = "DisplayBox"
+        Me.DisplayBox.Size = New System.Drawing.Size(361, 438)
+        Me.DisplayBox.TabIndex = 1
+        Me.DisplayBox.TabStop = False
+        Me.DisplayBox.Text = "Address Display"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(24, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(24, 96)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(24, 139)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(24, 179)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox4.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Name, First & Last"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Address 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(24, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "City"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(21, 163)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Zip/Postal Code"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(23, 202)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "State"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(26, 218)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox5.TabIndex = 8
         '
         'AddressLable
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DisplayBox)
+        Me.Controls.Add(Me.UserInputBox)
         Me.Name = "AddressLable"
         Me.Text = "Form1"
+        Me.UserInputBox.ResumeLayout(False)
+        Me.UserInputBox.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents UserInputBox As GroupBox
+    Friend WithEvents DisplayBox As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
 End Class
--- AddressLable/AddressLable.vb
+++ AddressLable/AddressLable.vb
@@ -6,6 +6,24 @@
 Option Strict On
 Option Explicit On
 Public Class AddressLable
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub DisplayBox_Enter(sender As Object, e As EventArgs) Handles DisplayBox.Enter
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
 'https://elearn.isu.edu/moodle/mod/assign/view.php?id=2180790
